c5547f8ebf743bb2bdce48a603f8f79e35fd539a
Rob Whitby <rob.whitby@springer.com>
2018-03-07 19:49:39 +0000

    add a canvas based on [][]rune

--- canvas.go
+++ canvas.go
@@ -6,15 +6,14 @@ type Canvas interface {
 	Contains(x, y int) bool
 	Get(x, y int) rune
 	Set(x, y int, r rune)
-	Recreate(x, y int)
+	Recreate(width, height int)
 }
 
 func NewCanvas(width int, height int) Canvas {
-	return &MapCanvas{
-		width:  width,
-		height: height,
-		points: make(map[point]rune),
-	}
+	//canvas := &MapCanvas{}
+	canvas := &ArrayCanvas{}
+	canvas.Recreate(width, height)
+	return canvas
 }
 
 type point struct {
@@ -55,4 +54,44 @@ func (c *MapCanvas) Recreate(width, height int) {
 	c.width = width
 	c.height = height
 	c.points = make(map[point]rune)
+}
+
+// array canvas
+
+type ArrayCanvas struct {
+	width  int
+	height int
+	points [][]rune
+}
+
+func (c *ArrayCanvas) Width() int {
+	return c.width
+}
+
+func (c *ArrayCanvas) Height() int {
+	return c.height
+}
+
+func (c *ArrayCanvas) Contains(x, y int) bool {
+	return x > 0 && x <= c.width && y > 0 && y <= c.height
+}
+
+func (c *ArrayCanvas) Get(x, y int) rune {
+	return c.points[y-1][x-1]
+}
+
+func (c *ArrayCanvas) Set(x, y int, r rune) {
+	c.points[y-1][x-1] = r
+}
+
+func (c *ArrayCanvas) Recreate(width, height int) {
+	c.width = width
+	c.height = height
+	c.points = make([][]rune, height)
+	for y := range c.points {
+		c.points[y] = make([]rune, width)
+		for x := range c.points[y] {
+			c.points[y][x] = ' '
+		}
+	}
 }
